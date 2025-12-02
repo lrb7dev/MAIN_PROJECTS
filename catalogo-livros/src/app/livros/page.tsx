e1309a9eabd1b1d1739cd90b94d56d0269420f58
lrb7dev <luanrb.dev@gmail.com>
2025-12-02 12:17:23 -0300

    Estrutura inicial do Catálogo de Livros

---
+++ catalogo-livros/src/app/livros/page.tsx
@@ -0,0 +1,8 @@
+export default function LivrosPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Meus Livros</h1>
+      <p>Nenhum livro cadastrado ainda.</p>
+    </div>
+  );
+}
