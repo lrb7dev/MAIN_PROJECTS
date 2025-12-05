1878fc1d6f3de3185e8c39044e9c50195ef51c0c
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2025-12-05 18:23:44 -0300

    Delete catalogo-livros directory

--- catalogo-livros/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Catálogo de Livros</h1>
-      <p>Bem-vindo ao seu gerenciador de leituras!</p>
-    </div>
-  );
-}
